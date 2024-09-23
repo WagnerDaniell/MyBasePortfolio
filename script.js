b2de07f365d44c4655176d61200ea00f2ecd9dce
WagnerDaniell <160763405+WagnerDaniell@users.noreply.github.com>
2024-09-23 11:26:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+//Função para navegar da home para sobre
+function pag_sobre() {
+    window.location.href = "pages/sobre/sobre.html";
+}
+function pag_contato() {
+    window.location.href = "pages/contato/contato.html";
+}
+//Função para navegar para home
+function pag_home () {
+    window.location.href = "../../index.html";
+}
